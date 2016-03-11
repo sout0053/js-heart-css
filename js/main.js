82f2bbbc21676117f54b183e403addfee6e2f97a
sout0053 <maxwell.southwood@outlook.com>
2016-03-11 09:59:38 -0500

    Began adding javascript

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main')
+var $p1 = $('main p:first-child');
+var $h2 = $('h2');
+var $li = $('li');
+var $img = $('img');
+
+var $dl = $('dl');
+var $dt = $('dt');
+
+$header.addClass('masthead');
+$h1.removeClass('big-heading');
+$main.addClass('wrapper');
+$p1.addClass('intro');
+$h2.addClass('slug-head');
+$li.addClass('slug-list');
+$img.addClass('slug-img');
+
+$dl.addClass('classification');
+$dt.removeClass('big-label');
