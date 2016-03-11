d9e384f06a5fbbf2753c46ad33f41bed5326d713
sout0053 <maxwell.southwood@outlook.com>
2016-03-11 10:17:45 -0500

    Finished adding javascript

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@ var $p1 = $('main p:first-child');
 var $h2 = $('h2');
 var $li = $('li');
 var $img = $('img');
-
+var $firstli = $('li:first-child');
+var $lastli = $('li:last-child');
 var $dl = $('dl');
 var $dt = $('dt');
+var $lastli2 = $('li:last-child');
 
 $header.addClass('masthead');
 $h1.removeClass('big-heading');
@@ -16,6 +18,8 @@ $p1.addClass('intro');
 $h2.addClass('slug-head');
 $li.addClass('slug-list');
 $img.addClass('slug-img');
-
+$firstli.addClass('slug-list-first');
+$lastli.addClass('slug-list-last');
 $dl.addClass('classification');
 $dt.removeClass('big-label');
+$lastli2.show();
